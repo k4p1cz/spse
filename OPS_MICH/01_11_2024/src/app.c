33a9db7ea89a1566236b5572396ad4e72dcb2a7e
Teodor Kapitanov <xangloomy@gmail.com>
2024-01-11 11:46:49 +0100

    .

--- OPS_MICH/01_11_2024/src/app.c
+++ OPS_MICH/01_11_2024/src/app.c
@@ -13,6 +13,7 @@
 #include "stm32_kit/lcd.h"
 #include "stm32_kit/led.h"
 #include "stm32_kit/keypad.h"
+#include "stm32_kit/button.h"
 
 #ifndef KEYPAD_STYLE
   static uint8_t KBD_MAP[KEYPAD_ROWS][KEYPAD_COLS] = {      // Defaultni rozlozeni pro 4x4 KeyPad
@@ -39,78 +40,145 @@
 	
 uint8_t waitForKeyPress(void);
 __task void timer(void);
+__task void button(void);
+__task void buttonPause(void);
+int getTimeFromUser(int e);
 	
 
 OS_TID id_timer;
 OS_TID id_timerSetup;
 OS_TID id_lcd;
 OS_TID id_keypad;
+OS_TID id_button;
+OS_TID id_buttonPause;
 
+OS_SEM semaphore;
 	
+int workTimer = 0;
+int restTimer = 0;
 int timerSeconds = 0;
+int cycle = 0; // 0 work , 1 rest
+int pause = 0; 
 	
 	
 
 #define LCD_8x2 (((LCD_COLS) == 8) && ((LCD_ROWS) == 2))
 
-__task void Keypad(){
+__task void buttonPause(){
 	for(;;){
-		
-	}
-}
-__task void LCD(){
-	for(;;){
-		
+		if(io_read(USER_BUTTON)){
+			pause ^= 1;
+			int led = pause;
+			led ^= 1;
+			io_set(cycle?LED_EX_0 : LED_EX_3, pause);
+			io_set(LED_EX_2, led);
+			io_set(LED_EX_1, led);
+			LCD_print("PAUSED");
+			delay(1000);
+		}
 	}
 }
 
 __task void timerSetup(){
+	workTimer = getTimeFromUser(0);
+	restTimer = getTimeFromUser(1);
+	timerSeconds = workTimer;
+	
+	id_button = os_tsk_create(button, 0);
+	os_tsk_delete_self();
+}
+
+
+__task void button(void){
+	for(;;){
+		if(io_read(USER_BUTTON)){
+			id_timer = os_tsk_create(timer, 0);
+			id_buttonPause = os_tsk_create(buttonPause, 0);
+			os_tsk_delete_self();
+		}
+	}
+}
+
+__task void timer(void){
+	char l1[9];
+	int mins, secs;
+	
+	io_set(LED_EX_0, 1);
+	io_set(LED_EX_3, 1);
+	io_set(cycle?LED_EX_0 : LED_EX_3, 0);
+	
+	for(;timerSeconds;timerSeconds--){
+		while(pause);
+		
+		mins = timerSeconds/60;
+		secs = timerSeconds%60;
+		LCD_set(LCD_LINE1);
+		LCD_print(cycle?"REST     ":"WORK      ");
+		LCD_print("ZBYVA:   ");
+		LCD_set(LCD_LINE2);
+		snprintf(l1, 9, "%d:%d      ", mins, secs);
+		LCD_print(l1);
+		
+		delay_ms(1000);
+	}
+	//id_timer = os_tsk_create(timer, 0);
+	if(cycle == 0){
+		cycle = 1;
+		timerSeconds = restTimer;
+	}else{
+		cycle = 0;
+		timerSeconds = workTimer;
+	}
+	id_timer = os_tsk_create(timer, 0);
+	os_tsk_delete_self();
+}
+
+int getTimeFromUser(int e){
 	uint8_t ch;
 	int mins, secs;
 	char time[8];
 	char *timeP = time;
 	int l = 0;
+	LCD_set(LCD_CLR);
 	LCD_set(LCD_CUR_OFF);
 	LCD_set(LCD_CUR_NO_BLINK);
-	LCD_set(LCD_LINE1);
-	LCD_print("CAS:      ");
-	LCD_set(LCD_LINE2);
-	for(;;){
-		for(l=0;;l++){
-			ch = waitForKeyPress();
-			if(ch == '#') break;
-			LCD_symbol(ch);
-			delay_ms(100);
-			*timeP++ = ch;
-			l++;
-		}
-		
-		char *tok;
-		
-		tok = strtok(time, ":");
-		int j = 0;
-		while(tok != NULL){
-			if(j == 0){
-				mins = atoi(tok);
-				j++;
-			}else if(j == 1){
-				secs = atoi(tok);
-				j++;
-			}
-			tok = strtok(NULL, ";");
-		}
-		LCD_set(LCD_LINE2);
-		snprintf(time, 8, "%dm %ds", mins, secs);
-		timerSeconds = mins*60 + secs;
-		LCD_print(time);
-		delay_ms(1000);
-		id_timer = os_tsk_create(timer, 0);
-		os_tsk_delete_self();
-	}
-}
-
-__task void timer(void){
 	
+	LCD_set(LCD_LINE1);
+	if(e == 0) LCD_print("CAS WORK:");
+	else LCD_print("CAS REST:");
+	
+	LCD_set(LCD_LINE2);
+	
+	for(;;){
+		ch = waitForKeyPress();
+		if(ch == '#') break;
+		LCD_symbol(ch);
+		delay_ms(100);
+		*timeP++ = ch;
+		l++;
+	}
+	
+	LCD_set(LCD_CLR);
+	LCD_set(LCD_LINE1);
+	LCD_print("ZMACKNI ");
+	LCD_set(LCD_LINE2);
+	LCD_print("TLACITKO");
+
+	char *tok;
+		
+	tok = strtok(time, ":");
+	int j = 0;
+	while(tok != NULL){
+		if(j == 0){
+			mins = atoi(tok);
+			j++;
+		}else if(j == 1){
+			secs = atoi(tok);
+			j++;
+		}
+		tok = strtok(NULL, ";");
+	}
+	return mins*60 + secs;
 }
 
 uint8_t waitForKeyPress(void){
@@ -126,9 +194,10 @@ __task void setup() {
 	KBD_setup();
 	LCD_setup();
 	LED_setup();
+	BTN_setup();
 
-	//id_keypad = os_tsk_create(Keypad, 0);
-	//id_lcd = os_tsk_create(LCD, 0);
+	os_sem_init(&semaphore, 0);
+	os_sem_send(&semaphore);
 	id_timerSetup = os_tsk_create(timerSetup, 0);
 	os_tsk_delete_self();
 }

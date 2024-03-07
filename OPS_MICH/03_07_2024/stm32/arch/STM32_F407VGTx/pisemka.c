4920ffaa7312d892f3c411476ee5661336b7419b
Teodor Kapitanov <xangloomy@gmail.com>
2024-03-07 09:59:35 +0100

    Update pisemka.c

--- OPS_MICH/03_07_2024/stm32/arch/STM32_F407VGTx/pisemka.c
+++ OPS_MICH/03_07_2024/stm32/arch/STM32_F407VGTx/pisemka.c
@@ -6,12 +6,31 @@
 
 OS_TID uartProcess;
 
+typedef struct{
+	int ip[4];
+	int mask[4];
+	int blockSize;
+	int numOfAddress;
+	int numOfHosts;
+	int hosts[2]; // 1. a posledni host
+	int networkIP[4];
+	int broadcastIP[4];
+	int CIDR;
+}addressInfo;
+
+char newLine[100] = "\r\n";
+char clear[100] = "\033[2J\033[1;1H";
+
 __task void uartProcessFunc(){
 	char buff[999];
 	int buff_len = 0;
 	uint8_t e;
 	int numOfArgs = 0;
+	int enteredDetails = 0;
+	int enteredIP = 0;
+	int enteredMask = 0;
 	char cmd[100];
+	char ip[100];
 	for(;;){
 		e = UART_getc(); // nactu znak
 		switch(e){
@@ -20,10 +39,24 @@ __task void uartProcessFunc(){
 				buff[buff_len] = 0x00;
 			break;
 			case 0x0D: // enter
-				
-			
-			
-			
+				if(strcmp(buff, "clear") == 0){
+					UART_write(clear, strlen(clear));
+				}
+				if(enteredDetails == 0){
+					numOfArgs = sscanf(buff, "%s %s", cmd, ip); // rozdelim si buffer do nekolika casti
+					if(numOfArgs > 1){
+						if(0 == strcmp(cmd, "ip")){ // overuju, jestli command, co uzivatel zadal je ip
+							UART_write(newLine, strlen(newLine));
+							enteredIP = 1;
+							if(enteredIP == 1 && enteredMask == 1){
+								// process
+							}
+						}
+						if(0 == strcmp(cmd, "mask")){
+						
+						}
+					}
+				}
 				// vycistim si pole buffer a pole command
 				memset(buff, 0x00, 999);
 				memset(cmd, 0x00, 100);
@@ -39,7 +72,7 @@ __task void uartProcessFunc(){
 
 __task void setup() {
 	UART_setup();
-
+	UART_write(clear, strlen(clear));
 	uartProcess = os_tsk_create(uartProcessFunc, 0); // vytvorim task
 	os_tsk_delete_self();
 }

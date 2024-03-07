3e2ad32d96bec07d76e04dca8a3de052cf124d33
Teodor Kapitanov <xangloomy@gmail.com>
2024-03-07 09:49:29 +0100

    Update pisemka.c

--- OPS_MICH/03_07_2024/stm32/arch/STM32_F407VGTx/pisemka.c
+++ OPS_MICH/03_07_2024/stm32/arch/STM32_F407VGTx/pisemka.c
@@ -4,12 +4,43 @@
 #include <string.h>
 #include <stdlib.h>
 
+OS_TID uartProcess;
 
-
+__task void uartProcessFunc(){
+	char buff[999];
+	int buff_len = 0;
+	uint8_t e;
+	int numOfArgs = 0;
+	char cmd[100];
+	for(;;){
+		e = UART_getc(); // nactu znak
+		switch(e){
+			case '\b': // backspace - NEMUSI FUNGOVAT 100%
+				buff_len--;
+				buff[buff_len] = 0x00;
+			break;
+			case 0x0D: // enter
+				
+			
+			
+			
+				// vycistim si pole buffer a pole command
+				memset(buff, 0x00, 999);
+				memset(cmd, 0x00, 100);
+			break;
+			default: // pokud ne backspace ani enter - vypsat znak a ulozit si ho do bufferu
+				UART_putc(e);
+				buff[buff_len] = e;
+				buff_len++;
+				break;
+		}
+	}
+}
 
 __task void setup() {
 	UART_setup();
 
+	uartProcess = os_tsk_create(uartProcessFunc, 0); // vytvorim task
 	os_tsk_delete_self();
 }
 int main () {
